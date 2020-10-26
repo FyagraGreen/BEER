2e68c1da6ff677ae6ad14330dc877a303f80af10
Павел Редькин <redkin_pasha@inbox.ru>
2020-10-26 13:23:28 +0200

    Я добавил строчку кода  Console.WriteLine("Тут был я, наверно");

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.Write("Введите число a: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+            Console.WriteLine("ывапывапм");
+            Console.WriteLine("Тут был я, наверно");
+            Console.ReadLine();
+        }
+    }
+}
